708f261c71b504506b9cbc75421263e9d3f673dc
Anmol Gorakshakar <anmol_gorakshakar@Anmols-MBP.lan>
2020-09-15 23:25:48 -0400

    added doi based search

--- nlp/elsapy_connector.py
+++ nlp/elsapy_connector.py
@@ -30,4 +30,28 @@ class elsapy_connector():
 
             if not count%20:
                 print (f'{count} articles processed')
+        self.pii_result = pd.DataFrame(res)
+        return res
+
+    def doi_search(self, df, rows = None):
+        count = 0
+        res = []
+        if rows:
+            df = df.loc[:rows-1]
+        doi = df['prism:doi'].dropna()
+        for i in doi:
+            data = FullDoc(doi = i)
+            if data.read(client):
+                if type(data.data['originalText']) == str:
+                    res.append([i, data, 'paper found'])
+                else:
+                    res.append([i, data, 'No full text article found'])
+                count += 1
+            else:
+                res.append([i, data, 'No article found'])
+                count += 1
+
+            if not count%50:
+                print (f'{count} articles processed')
+        self.doi_result = pd.DataFrame(res)
         return res
