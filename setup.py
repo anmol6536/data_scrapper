1ee2431bf9d4d92b4ed412329ee0e716fa70bba4
Anmol Gorakshakar <anmol_gorakshakar@Anmols-MBP.lan>
2020-09-15 20:11:40 -0400

    name change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="nlp_hr", # Replace with your own username
     version="0.0.1",
     author="Anmol Gorakashakar",
     author_email="anmol@genecentrix.com",
