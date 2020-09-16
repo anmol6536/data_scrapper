79e4f29b54ea26f23e1a6cd615896f69a89693a3
Anmol Gorakshakar <anmol_gorakshakar@Anmols-MBP.lan>
2020-09-15 20:46:27 -0400

    fixed error which caused urls to be generated twice if the number of urls to generate was less than 25

--- nlp/requests.py
+++ nlp/requests.py
@@ -2,6 +2,7 @@ import requests
 import json
 import string
 import pandas as pd
+from math import ceil
 
 class request_handler():
     def __init__(self):
@@ -25,12 +26,16 @@ class request_handler():
 
     def get_urls(self,search_string, total_results = 25 ):
         #url generation
-        base = f'https://api.elsevier.com/content/search/scopus?start=0&count=25&query={search_string}&apiKey={self.apikey}'
         start=0
-        end=round(total_results/25)
+        end=ceil(total_results/25)
+        final_count = total_results%25
+        count = 25
         urls = []
-        for i in range(end+1):
-            url = f'https://api.elsevier.com/content/search/scopus?start={start}&count=25&query={search_string}&apiKey={self.apikey}'
+        for i in range(end):
+            print (i, end)
+            if i == end-1:
+                count = final_count
+            url = f'https://api.elsevier.com/content/search/scopus?start={start}&count={count}&query={search_string}&apiKey={self.apikey}'
             start+=25
             urls.append(url)
         return urls
