6d91a4fa32eff81d1e4e6ee4157c9cee7c75785d
Anmol Gorakshakar <anmol_gorakshakar@Anmols-MBP.lan>
2020-10-06 14:58:25 -0400

    init update

--- allergan/__init__.py
+++ allergan/__init__.py
@@ -2,4 +2,4 @@ from .requests import request_handler
 from .index import final_result
 from .plots import overview
 from .elsapy_connector import elsapy_connector
-from barcode_generator import *
+from .barcode_generator import *
