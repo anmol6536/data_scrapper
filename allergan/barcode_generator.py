650f2f70c7680a8c2a17f06d4b5fb488fc2cf6de
Anmol Gorakshakar <anmol_gorakshakar@Anmols-MBP.lan>
2020-10-06 14:50:51 -0400

    snall change

--- allergan/barcode_generator.py
+++ allergan/barcode_generator.py
@@ -21,9 +21,7 @@ class pl:
                 """
         return query
 
-    def biogps_plotter(gene, connection):
-        if not connection:
-            connection = cnx_hr
+    def biogps_plotter(gene, connection=cnx_hr):
         query = query_generator(gene)
         df = pd.read_sql_query(query, connection).drop(["index", "symbol"], axis=1)
 
