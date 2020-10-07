b7b05682d7d82bde01dbe8ac72b3a08940d34798
Anmol Gorakshakar <anmol_gorakshakar@Anmols-MBP.lan>
2020-10-07 12:03:54 -0400

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="allergan",  # Replace with your own username
-    version="0.0.7.06",
+    version="0.0.7.07",
     author="Anmol Gorakashakar",
     author_email="anmol@genecentrix.com",
     description="Data collection package",
